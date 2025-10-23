4115b37bbfc01b9b850778de8597c511abb7500b
‘Anastasiia <miss.iuferova@yandex.ru>
2025-10-23 15:04:01 +0200

    add: MenuItem methods

--- src/main/java/com/codefirstgirls/MenuItem.java
+++ src/main/java/com/codefirstgirls/MenuItem.java
@@ -1,11 +1,40 @@
 package com.codefirstgirls;
 
-public class MenuItem {
+public abstract class MenuItem {
   String name;
-  int price;
+  int basePrice;
+  int currentPrice;
 
   MenuItem(String name, int price){
     this.name =  name;
-    this.price = price;
+    this.basePrice = price;
+    this.currentPrice = basePrice;
+  }
+
+  abstract void getMenuItemSlogan();
+
+  public void makeDiscount(){
+    CurrentDiscount currentDiscount = new CurrentDiscount();
+    int discountValue = currentDiscount.getDiscount();
+
+    if(discountValue > 0){
+      System.out.println("Your discount is " + discountValue + "%!");
+    }
+    else{
+      System.out.println("Price is not discounted at the time");
+    }
+    this.currentPrice = (int) (basePrice - (basePrice * (discountValue * 0.01)));
+  }
+
+  public void removeDiscount(){
+    this.currentPrice = this.basePrice;
+  };
+
+  public int getBasePrice() {
+    return basePrice;
+  }
+
+  public void setBasePrice(int basePrice) {
+    this.basePrice = basePrice;
   }
 }
