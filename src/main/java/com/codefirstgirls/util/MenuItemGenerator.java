e3112a6420dd8c9fd175e1ef98e7dcedabdb5449
‘Anastasiia <miss.iuferova@yandex.ru>
2025-10-24 17:10:00 +0200

    feat: MenuItemGenerator

--- src/main/java/com/codefirstgirls/util/MenuItemGenerator.java
+++ src/main/java/com/codefirstgirls/util/MenuItemGenerator.java
@@ -1,14 +1,48 @@
 package com.codefirstgirls.util;
 
+import com.codefirstgirls.constants.MenuItemConstants;
+import com.codefirstgirls.menu.*;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Random;
+
 public class MenuItemGenerator {
 
+  private static MenuItem generateSingleMenuItem(MenuPosition type, String name, double price) {
+    return switch (type) {
+      case MenuPosition.DRINK -> new Drink(name, price);
+      case MenuPosition.MAIN_DISH -> new MainDish(name, price);
+      case MenuPosition.DESSERT -> new Dessert(name, price);
+    };
+  }
 
+  private static MenuItem getRandomNamePrice(MenuPosition type) {
+    Map<String, Double> positions = MenuItemConstants.getMenuPositions(type);
+    ArrayList<Map.Entry<String, Double>> entries = new ArrayList<>(positions.entrySet());
 
+    Random random = new Random();
+    int index = random.nextInt(entries.size());
 
+    Map.Entry<String, Double> entry = entries.get(index);
+    String name = entry.getKey();
+    double price = entry.getValue();
+    return generateSingleMenuItem(type, name, price);
+  }
 
-//  public static Fish randomFish(){
-//    String fishName = names[random.nextInt(names.length)];
-//    int age = random.nextInt(10);
-//    return new Fish(fishName, age);
-//  }
+  public static MenuItem[] generateMultipleMenuItems(String type, int number){
+    MenuItem[] menuItemArray = new MenuItem[number];
+
+    for (int i = 0; i < number; i++){
+        menuItemArray[i] = getRandomNamePrice(MenuPosition.valueOf(type));
+      }
+      return menuItemArray;
+  }
 }
+
+
+
+
+
+
+
